09ee1678a1c79a2ac9d22d58357a129288072412
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2022-10-02 06:17:54 -0700

    cambios

--- API.py
+++ API.py
@@ -1,56 +1,47 @@
 import json
 import random
 import requests
-import tensorflow as tf
-import Modelo as Modelo
 import base64
+from io import BytesIO
+from Modelo import Model
 from flask import Flask, request
+
+m = Model()
 
 app = Flask(__name__)
 
 
 @app.route('/generateImage', methods=['GET'])
 def generate_image():
-    searchword = request.args.get('key', '')
-    url = "https://images-api.nasa.gov/search?q=" + searchword + "&media_type=image"
+    search_word = request.args.get('key', '')
+    url = "https://images-api.nasa.gov/search?q=" + search_word + "&media_type=image"
 
     res = requests.get(url)
     response = json.loads(res.text)
 
-    res_art = requests.get("https://openaccess-api.clevelandart.org/api/artworks/")
-    response_art = json.loads(res_art.text)
-    # art_url = str(response_art["data"][random.randint(0, 999)]["images"]["web"]["url"])
+    art_url = "Art/" + str(random.randint(1, 26)) + ".jpg"
 
-    art_url="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpaperset.com%2Fw%2Ffull%2F2%2F2%2F9%2F207342.jpg&f=1&nofb=1&ipt=2fda1fa6e3b7fd801376864f512323acd52519acb2711b81b5fcc18ae8889151&ipo=images"
     if len(response["collection"]["items"]) == 0:
-        searchword = "space"
-        url = "https://images-api.nasa.gov/search?q=" + searchword + "&media_type=image"
+        search_word = "space"
+        url = "https://images-api.nasa.gov/search?q=" + search_word + "&media_type=image"
         res = requests.get(url)
         response = json.loads(res.text)
 
     index = random.randint(0, len(response["collection"]["items"])) - 1
     imagen_url = str(response["collection"]["items"][index]["links"][0]["href"])
 
-    print("PROCESANDO REQUEST")
-    m = Modelo.ArtModel(imagen_url, art_url)
-    m.download_image(m.url, "C:/Users/Usuario/Desktop/spaceapps/imagenes/", m.file_name)
-    m.download_image(m.art_url, "C:/Users/Usuario/Desktop/spaceapps/imagenes/", "Arte")
+    print('Content: {content}'.format(content=imagen_url))
+    print('Style: {style}'.format(style=art_url))
 
-    m.content = m.load_file(m.content_path)
-    m.style = m.load_file(m.style_path)
-    m.im = m.img_preprocess(m.content_path)
-    m.im2 = m.deprocess_img(m.im)
-    m.model.summary()
-    m.model = m.get_model()
-    m.model.summary()
-    m.model = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
-    m.best, m.best_loss, m.image = m.run_style_transfer(m.content_path, m.style_path, epochs=2500)
+    image = m.generate_image(str(random.randint(0, 999)) + ".jpg",
+                             imagen_url,
+                             art_url)
 
-    with open("imagenes/1.jpg", "rb") as image2string:
-        converted_string = base64.b64encode(image2string.read())
-    print(converted_string)
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue())
 
-    return converted_string
+    return str(img_str)
 
 
-app.run(debug=True)
+app.run(debug=False)
--- Modelo.py
+++ Modelo.py
@@ -1,204 +1,49 @@
+import os
 import tensorflow as tf
+import matplotlib as mpl
+import tensorflow_hub as hub
 import numpy as np
-import tensorflow.keras.preprocessing.image as process_im
-import matplotlib.pyplot as plt
-import IPython.display
-import urllib.request
-from tensorflow.python.keras import models
-from PIL import Image
+import PIL.Image
 
 
-class ArtModel:
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
 
-    def download_image(self, url, file_path, file_name):
-        full_path = file_path + file_name + '.jpg'
-        urllib.request.urlretrieve(url, full_path)
 
-    def load_file(self, image_path):
-        image = Image.open(image_path)
+class Model:
+    def tensor_to_image(self, tensor):
+        tensor = tensor * 255
+        tensor = np.array(tensor, dtype=np.uint8)
+        if np.ndim(tensor) > 3:
+            assert tensor.shape[0] == 1
+            tensor = tensor[0]
+        return PIL.Image.fromarray(tensor)
+
+    def load_img(self, path_to_img):
         max_dim = 512
-        factor = max_dim / max(image.size)
-        image = image.resize((round(image.size[0] * factor), round(image.size[1] * factor)), Image.ANTIALIAS)
-        im_array = process_im.img_to_array(image)
-        im_array = np.expand_dims(im_array, axis=0)
-        return im_array
+        img = tf.io.read_file(path_to_img)
+        img = tf.image.decode_image(img, channels=3)
+        img = tf.image.convert_image_dtype(img, tf.float32)
 
-    def show_im(self, img, title=None):
-        img = np.squeeze(img, axis=0)
-        plt.imshow(np.uint8(img))
-        if title is None:
-            pass
-        else:
-            plt.title(title)
-        plt.imshow(np.uint8(img))
+        shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+        long_dim = max(shape)
+        scale = max_dim / long_dim
 
-    # %%103.939
-    def img_preprocess(self, img_path):
-        images = self.load_file(img_path)
-        img = tf.keras.applications.vgg19.preprocess_input(images)
+        new_shape = tf.cast(shape * scale, tf.int32)
+
+        img = tf.image.resize(img, new_shape)
+        img = img[tf.newaxis, :]
         return img
 
-    def deprocess_img(self, processed_img):
-        x = processed_img.copy()
-        if len(x.shape) == 4:
-            x = np.squeeze(x, 0)
-        assert len(x.shape) == 3
+    def generate_image(self, content_name, content_url, style_url):
+        content_path = tf.keras.utils.get_file(content_name, content_url)
+        content_image = self.load_img(content_path)
 
-        x[:, :, 0] += 105
-        x[:, :, 1] += 116.779
-        x[:, :, 2] += 123.68
-        x = x[:, :, ::-1]
+        style_image = self.load_img(style_url)
 
-        x = np.clip(x, 0, 255).astype('uint8')
-        return x
+        hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+        stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+        img = self.tensor_to_image(stylized_image)
 
-    def get_model(self):
-        vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
-        vgg.trainable = False
-        content_output = [vgg.get_layer(layer).output for layer in self.content_layers]
-        style_output = [vgg.get_layer(layer).output for layer in self.style_layers]
-        model_output = style_output + content_output
-        return models.Model(vgg.input, model_output)
-
-
-    def get_content_loss(self, noise, target):
-        loss = tf.reduce_mean(tf.square(noise - target))
-        return loss
-
-    def gram_matrix(self, tensor):
-        channels = int(tensor.shape[-1])
-        vector = tf.reshape(tensor, [-1, channels])
-        n = tf.shape(vector)[0]
-        gram_matrix = tf.matmul(vector, vector, transpose_a=True)
-        return gram_matrix / tf.cast(n, tf.float32)
-
-    def get_style_loss(self, noise, target):
-        gram_noise = self.gram_matrix(noise)
-        # gram_target=gram_matrix(target)
-        loss = tf.reduce_mean(tf.square(target - gram_noise))
-        return loss
-
-    def get_features(self, model, content_path, style_path):
-        content_img = self.img_preprocess(content_path)
-        style_image = self.img_preprocess(style_path)
-
-        content_output = model(content_img)
-        style_output = model(style_image)
-
-        content_feature = [layer[0] for layer in content_output[self.number_style:]]
-        style_feature = [layer[0] for layer in style_output[:self.number_style]]
-        return content_feature, style_feature
-
-
-    def compute_loss(self, model, loss_weights, image, gram_style_features, content_features):
-        style_weight, content_weight = loss_weights
-        output = model(image)
-        content_loss = 0
-        style_loss = 0
-
-        noise_style_features = output[:self.number_style]
-        noise_content_feature = output[self.number_style:]
-
-        weight_per_layer = 1.0 / float(self.number_style)
-        for a, b in zip(gram_style_features, noise_style_features):
-            style_loss += weight_per_layer * self.get_style_loss(b[0], a)
-
-        weight_per_layer = 1.0 / float(self.number_content)
-        for a, b in zip(noise_content_feature, content_features):
-            content_loss += weight_per_layer * self.get_content_loss(a[0], b)
-
-        style_loss *= style_weight
-        content_loss *= content_weight
-
-        total_loss = content_loss + style_loss
-        return total_loss, style_loss, content_loss
-
-
-    def compute_grads(self, dictionary):
-        with tf.GradientTape() as tape:
-            all_loss = self.compute_loss(**dictionary)
-
-        total_loss = all_loss[0]
-        return tape.gradient(total_loss, dictionary['image']), all_loss
-
-
-    # %%
-    def run_style_transfer(self, content_path, style_path, epochs=500, content_weight=1e3, style_weight=1e1):
-        model = self.get_model()
-
-        for layer in model.layers:
-            layer.trainable = False
-
-        content_feature, style_feature = self.get_features(model, content_path, style_path)
-        style_gram_matrix = [self.gram_matrix(feature) for feature in style_feature]
-
-        noise = self.img_preprocess(content_path)
-        noise = tf.Variable(noise, dtype=tf.float32)
-
-        optimizer = tf.keras.optimizers.Adam(learning_rate=5, beta_1=0.99, epsilon=1e-1)
-
-        best_loss, best_img = float('inf'), None
-
-        loss_weights = (style_weight, content_weight)
-        dictionary = {'model': model,
-                      'loss_weights': loss_weights,
-                      'image': noise,
-                      'gram_style_features': style_gram_matrix,
-                      'content_features': content_feature}
-        norm_means = np.array([105, 116.779, 123.68])
-        min_vals = -norm_means
-        max_vals = 255 - norm_means
-        image_no = 1
-        imgs = []
-        for i in range(epochs):
-            grad, all_loss = self.compute_grads(dictionary)
-            total_loss, style_loss, content_loss = all_loss
-            optimizer.apply_gradients([(grad, noise)])
-            clipped = tf.clip_by_value(noise, min_vals, max_vals)
-            noise.assign(clipped)
-            if total_loss < self.best_loss:
-                best_loss = total_loss
-                best_img = self.deprocess_img(noise.numpy())
-            if i % 10 == 0:
-                plot_img = noise.numpy()
-                plot_img = self.deprocess_img(plot_img)
-                imgs.append(plot_img)
-                m = Image.fromarray(plot_img)
-                IPython.display.clear_output(wait=True)
-                IPython.display.display_png(Image.fromarray(plot_img))
-                name = r'imagenes/' + str(image_no) + '.jpg'
-                m.save(name, 'JPEG')
-                image_no += 1
-                #break
-            print(i)
-        IPython.display.clear_output(wait=True)
-        return best_img, best_loss, imgs
-
-    def __init__(self, path, art_path):
-        self.url = path
-        self.path = path
-        self.art_url = art_path
-        self.file_name = "content"
-        self.content_path = 'imagenes/content.jpg'
-        self.style_path = 'imagenes/Arte.jpg'
-        self.content = []
-        self.style = []
-        self.im = []
-        self.im_2 = []
-
-        self.best = []
-        self.best_loss = []
-        self.image = []
-
-        self.content_layers = ['block1_conv2',
-                          'block2_conv2',
-                          'block3_conv2']
-        self.style_layers = ['block1_conv1',
-                        'block2_conv1',
-                        'block3_conv1',
-                        'block4_conv1',
-                        'block5_conv1']
-        self.number_content = len(self.content_layers)
-        self.number_style = len(self.style_layers)
-
-        self.model = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
+        return img
